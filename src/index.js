30d69ecfa38864157dc5598b46a5167b6ab98a0f
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-18 12:28:24 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+im
+let SDtomb = [
+    {
+      "nev": "Maxi Ultra",
+      "meret": "128"
+    },
+    {
+      "nev": "Maxi Ultra S",
+      "meret": "256"
+    },
+    {
+      "nev": "Maxi Ultra X",
+      "meret": "512"
+    },
+    {
+      "nev": "Átlagos SD kártya",
+      "meret": "128"
+    },
+    {
+      "nev": "Átlagos SD kártya 2",
+      "meret": "256"
+    },
+    {
+      "nev": "Átlagos SD kártya 2.1",
+      "meret": "256"
+    },
+    {
+      "nev": "Ólcsó microSD",
+      "meret": "32"
+    },
+    {
+      "nev": "Kevésbé olcsó microSD",
+      "meret": "64"
+    }
+  ]
+  
